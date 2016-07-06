9e1e2100dce608446d8ed176c5fc6cb94bb501a8
Lourdesmarco <lourdesmarco.design@gmail.com>
2016-07-06 17:09:02 +0200

    iot_ready styles

--- js/icons.js
+++ js/icons.js
@@ -30,9 +30,9 @@
 		$('ul li').filter(function(index) { return $(this).text() === "Incubated GEs/GEris"; }).closest('ul').addClass('incubated-ge');
 		$('ul li').filter(function(index) { return $(this).text() === "FIWARE GEis"; }).closest('ul').addClass('fiware-ge');
 		$('ul li').filter(function(index) { return $(this).text() === "FIWARE GEris"; }).closest('ul').addClass('compliant-ge');
-		$('ul li').filter(function(index) { return $(this).text() === "Archived GEis"; }).closest('ul').addClass('archived-geis');
+		$('ul li').filter(function(index) { return $(this).text() === "Deprecated GEis"; }).closest('ul').addClass('archived-geis');
     
-    $('ul li').filter(function(index) { return $(this).text() === "Archived GEis"; }).closest('div').parent().prev().addClass('archived-geis-bg');
+    $('ul li').filter(function(index) { return $(this).text() === "Deprecated GEis"; }).closest('div').parent().prev().addClass('archived-geis-bg');
     $('ul li').filter(function(index) { return $(this).text() === "FIWARE GEris"; }).closest('div').parent().prev().addClass('compliant-ge-bg');
     $('ul li').filter(function(index) { return $(this).text() === "Incubated GEs/GEris"; }).closest('div').parent().prev().addClass('incubated-ge-bg');
     $('ul li').filter(function(index) { return $(this).text() === "FIWARE GEis"; }).closest('div').parent().prev().addClass('fiware-ge-bg');
@@ -48,7 +48,7 @@
 		$('a[href$="incubated-gesgeris"]').closest('div').addClass('incubated-ge');
 		$('a[href$="fiware-geis"]').closest('div').addClass('fiware-ge');
 		$('a[href$="fiware-geris"]').closest('div').addClass('compliant-ge');
-		$('a[href$="archived-geis"]').closest('div').addClass('archived-geis');
+		$('a[href$="deprecated-geis"]').closest('div').addClass('archived-geis');
                 $('#block-views-exp-api-list-pag').closest('div').html('<a href="/whatsnew">Whatsnew</a>');
 		if($('article').hasClass('node-unpublished')){
                  $(".node-unpublished").append('<img class="img-notpublished" src="/sites/all/themes/fiwareSASS/images/not_published.png">');
@@ -58,8 +58,8 @@
   	        $('ul li').filter(function(index) { return $(this).text() === "Incubated GEs/GEris"; }).closest('ul').addClass('incubated-ge');
                 $('ul li').filter(function(index) { return $(this).text() === "FIWARE GEis"; }).closest('ul').addClass('fiware-ge');
                 $('ul li').filter(function(index) { return $(this).text() === "FIWARE GEris"; }).closest('ul').addClass('compliant-ge');
-		$('ul li').filter(function(index) { return $(this).text() === "Archived GEis"; }).closest('ul').addClass('archived-geis');
-                    $('ul li').filter(function(index) { return $(this).text() === "Archived GEis"; }).closest('div').parent().prev().addClass('archived-geis-bg');
+		$('ul li').filter(function(index) { return $(this).text() === "Depreated GEis"; }).closest('ul').addClass('archived-geis');
+                    $('ul li').filter(function(index) { return $(this).text() === "Deprecated GEis"; }).closest('div').parent().prev().addClass('archived-geis-bg');
     $('ul li').filter(function(index) { return $(this).text() === "FIWARE GEris"; }).closest('div').parent().prev().addClass('compliant-ge-bg');
     $('ul li').filter(function(index) { return $(this).text() === "Incubated GEs/GEris"; }).closest('div').parent().prev().addClass('incubated-ge-bg');
     $('ul li').filter(function(index) { return $(this).text() === "FIWARE GEis"; }).closest('div').parent().prev().addClass('fiware-ge-bg');         
