1855733017e7a6d457d3187dba5db96605a6cbf1
dmartr <root@catalogue-dev.novalocal>
2016-07-07 13:03:39 +0200

    minor bug fixes

--- template.php
+++ template.php
@@ -8,7 +8,8 @@ function fiwareSASS_form_alter(&$form, &$form_state, $form_id) {
 $form['filter']['#attributes']['placeholder'] = t('Filter...');
 $form['filter_bundles']['#attributes']['placeholder'] = t('Filter...');
 $form['filter_tools']['#attributes']['placeholder'] = t('Filter...');
-if($form_id == 'user_register_form'){
-drupal_set_message(t('Dear user, please note that the registration is required only for editing the contents of the Catalogue (e.g. GE implementation). If your intention is to know how to use FI-WARE you don\'t need to create an account because all the information you need is publicly available.'), 'error');
+if($form_id == 'user_login'){
+drupal_set_message(t('Dear user, please note that the registration is required only for editing the contents of the Catalogue (e.g. GE implementation). If your intention is to know how to use FIWARE you don\'t need to create an account because all the information you need is publicly available. The editors of GE implementations on the Catalogue must register on the <a href="forge.fiware.org">FIWARE Forge</a> and then request access to the administrator of the Catalogue'), 'error');
 }
+drupal_set_message(t(' Account credentials administration must be done via FIWARE Forgue portal'), 'error');
 }
